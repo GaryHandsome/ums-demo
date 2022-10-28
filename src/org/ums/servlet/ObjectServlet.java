2f35b92bc1e4aa1fd9e4968f60394c8c06e21a7e
zing173 <zhqianxin@qq.com>
2022-10-28 09:36:38 +0800

    Ajax-JSON

---
+++ src/org/ums/servlet/ObjectServlet.java
@@ -0,0 +1,31 @@
+package org.ums.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 处理 Ajax 异步请求，向客户端响应一个对象 - JSON字符串
+ *
+ * @Date 2022-10-27
+ * @Author zqx
+ */
+@WebServlet("/hello.do")
+public class ObjectServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out = resp.getWriter();
+
+        String json = "{'name':'zs','age':18}" ;
+
+        out.print(json);
+
+        out.flush();
+        out.close();
+
+    }
+}
