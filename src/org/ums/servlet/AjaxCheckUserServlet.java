d64d4e7797fa4ad7ce295b85bbaeaf22925f434f
zing173 <zhqianxin@qq.com>
2022-10-31 12:00:59 +0800

    用户注册

---
+++ src/org/ums/servlet/AjaxCheckUserServlet.java
@@ -0,0 +1,52 @@
+package org.ums.servlet;
+
+import com.google.gson.Gson;
+import org.ums.dao.UserInfoDao;
+import org.ums.dao.impl.UserInfoDaoImpl;
+import org.ums.entity.UserInfo;
+import org.ums.vo.ResponseData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 异步请求实现检查用户是否已经被注册
+ *
+ * @Date 2022-10-27
+ * @Author zqx
+ */
+@WebServlet("/check_user.do")
+public class AjaxCheckUserServlet extends BaseServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 第一：获取表单中输入的数据
+        String uname = req.getParameter("username");
+
+        // 第二：加工处理 - 通过 DAO 对象，查询数据
+        UserInfoDao userInfoDao = new UserInfoDaoImpl();
+        UserInfo user = userInfoDao.selectUserInfoByName(uname);
+
+        // 第三：定义响应对象，封装响应数据
+        ResponseData responseData = null;
+
+        if (user == null) {
+            responseData = error("此帐号未注册，可用");
+        } else {
+            responseData = success("此帐号已注册，不可用");
+        }
+
+        // 第四：把响应数据转化为 JSON 字符串，并打印输出
+        String json = new Gson().toJson(responseData);
+        resp.setContentType("application/json;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        out.print(json);
+
+        out.flush();
+        out.close();
+
+    }
+}
---
+++ web/js/register.js
@@ -0,0 +1,23 @@
+// 当用户文本框失去焦点时，触发此事件 - 验证用户是否已经注册
+$("#uname").blur(function (){
+    // 获取当前文本框输入的帐号
+    // var name =  $(this).val() ;
+
+    // 使用 JQ 向服务器发起异步请求
+    $.ajax({
+        // 请求服务器的 URL 地址（Servlet）
+        url:'check_user.do',
+        // 指定请求的类型
+        type:'post',
+        // 向服务器传递的数据
+        data:'username=' +  $(this).val()  ,
+        // 设置服务器响应数据的类型
+        dataType:'json',
+        // 设置成功响应的回调函数
+        success:function( res ) {
+            console.log( res )
+        }
+    }) ;
+
+
+}) ;
